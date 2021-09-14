0e6eae3a23b6678fe35e0ec482737cb6ad85e4c7
wnma3mz <wnma3mz@163.com>
2021-09-14 17:39:43 +0800

    first commit moon reproduction

---
+++ example/MOON_reproduction/FedMOON.py
@@ -0,0 +1,124 @@
+# coding: utf-8
+
+import base64
+import copy
+import pickle
+import numpy as np
+
+import torch.nn as nn
+from flearn.client import Client
+from flearn.server import Server
+from flearn.client.train import Trainer
+import torch
+
+
+class MOONServer(Server):
+    def evaluate(self, data_lst, is_select=False):
+        # 仅测试一个客户端，因为每个客户端模型一致
+        if is_select == True:
+            return [data_lst[0]]
+
+        test_acc_lst = np.mean(list(map(lambda x: x["test_acc"], data_lst)), axis=0)
+        test_acc = "; ".join("{:.4f}".format(x) for x in test_acc_lst)
+        return test_acc
+
+
+class MOONTrainer(Trainer):
+    """"搭配MOONClient使用"""
+
+    def __init__(self, model, optimizer, criterion, device, display=True):
+        super(MOONTrainer, self).__init__(model, optimizer, criterion, device, display)
+        self.global_model = None
+        self.previous_model_lst = []
+        self.cos = nn.CosineSimilarity(dim=-1)
+        # CIFAR-10, CIFAR-100, and Tiny-Imagenet are 0.5, 1, and 0.5
+        self.temperature = 0.5
+        #  CIFAR-10, CIFAR-100, and Tiny-Imagenet are 5, 1, and 1
+        self.mu = 5
+
+    def _display_iteration(self, data_loader, is_train=True):
+        loop_loss = []
+        accuracy = []
+        for data, target in data_loader:
+            data, target = data.to(self.device), target.to(self.device)
+            _, pro1, output = self.model(data)
+            loss = self.criterion(output, target)
+            if self.display:
+                data_loader.postfix = "loss: {:.4f}".format(loss.data.item())
+            loop_loss.append(loss.data.item() / len(data_loader))
+            accuracy.append((output.data.max(1)[1] == target.data).sum().item())
+            if is_train:
+                self.optimizer.zero_grad()
+                if self.global_model != None:
+                    with torch.no_grad():
+                        # 全局与本地的对比损失，越小越好
+                        self.global_model.to(self.device)
+                        _, pro2, _ = self.global_model(data)
+                        posi = self.cos(pro1, pro2)
+                        logits = posi.reshape(-1, 1)
+
+                        # 当前轮与上一轮的对比损失，越大越好
+                        for previous_net in self.previous_model_lst:
+                            previous_net.to(self.device)
+                            _, pro3, _ = previous_net(data)
+                            nega = self.cos(pro1, pro3)
+                            logits = torch.cat((logits, nega.reshape(-1, 1)), dim=1)
+
+                    logits /= self.temperature
+                    labels = torch.zeros(data.size(0)).cuda().long()
+                    loss += self.mu * self.criterion(logits, labels)
+
+                loss.backward()
+                self.optimizer.step()
+        # 避免占用显存
+        # if is_train and self.global_model != None:
+        #     self.global_model.to("cpu")
+        #     for previous_net in self.previous_model_lst:
+        #         previous_net.to("cpu")
+
+        return loop_loss, accuracy
+
+
+class MOONClient(Client):
+    """FedMOONClient"""
+
+    def __init__(self, conf, pre_buffer_size=1):
+        # 保存以前模型的大小
+        self.pre_buffer_size = pre_buffer_size
+        super(MOONClient, self).__init__(conf)
+
+    def train(self, i):
+        self.train_loss, self.train_acc = self.model_trainer.loop(
+            self.epoch, self.trainloader
+        )
+        # 权重为本地数据大小
+        data_upload = self.strategy.client(
+            self.model_trainer, agg_weight=len(self.trainloader)
+        )
+        return self._pickle_model(data_upload)
+
+    def revice(self, i, glob_params):
+        # 额外需要两类模型，glob和previous，一般情况下glob只有一个，previous也定义只有一个
+        # 如果存储超过这个大小，则删除最老的模型
+        while len(self.model_trainer.previous_model_lst) >= self.pre_buffer_size:
+            self.model_trainer.previous_model_lst.pop(0)
+        self.model_trainer.previous_model_lst.append(
+            copy.deepcopy(self.model_trainer.model)
+        )
+
+        # decode
+        data_glob_b = self.encrypt.decode(glob_params)
+
+        # update
+        update_model = self.strategy.client_revice(self.model_trainer, data_glob_b)
+        if self.scheduler != None:
+            self.scheduler.step()
+        self.model_trainer.model = update_model
+        self.model_trainer.global_model = copy.deepcopy(update_model)
+
+        return {
+            "code": 200,
+            "msg": "Model update completed",
+            "client_id": self.client_id,
+            "round": str(i),
+        }
---
+++ example/MOON_reproduction/main.py
@@ -0,0 +1,165 @@
+# coding: utf-8
+import argparse
+import copy
+import os
+import random
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from flearn.client.utils import get_free_gpu_id
+from flearn.server import Communicate as sc
+
+from FedMOON import MOONClient, MOONServer, MOONTrainer
+from model import ModelFedCon
+from utils import get_dataloader, partition_data
+
+idx = get_free_gpu_id()
+print("使用{}号GPU".format(idx))
+if idx != -1:
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(idx)
+    torch.cuda.current_device()
+    torch.cuda._initialized = True
+else:
+    raise SystemError("No Free GPU Device")
+
+parser = argparse.ArgumentParser(description="Please input strategy_name")
+parser.add_argument("--strategy_name", dest="strategy_name")
+parser.add_argument("--local_epoch", dest="local_epoch", default=10, type=int)
+parser.add_argument("--frac", dest="frac", default=1, type=float)
+parser.add_argument("--suffix", dest="suffix", default="", type=str)
+parser.add_argument("--iid", dest="iid", action="store_true")
+parser.add_argument(
+    "--dataset_name",
+    dest="dataset_name",
+    default="cifar10",
+    choices=["tinyimagenet", "cifar10", "cifar100"],
+    type=str,
+)
+parser.add_argument(
+    "--dataset_fpath",
+    dest="dataset_fpath",
+    type=str,
+)
+
+args = parser.parse_args()
+iid = args.iid
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# 设置数据集
+dataset_name = args.dataset_name
+dataset_fpath = args.dataset_fpath
+batch_size = 64
+if iid == True:
+    partition = "homo"
+else:
+    partition = "noniid"
+    beta = 0.5
+print("切分{}数据集, 切割方式: {}".format(dataset_name, partition))
+
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    # tf.random.set_seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+
+# 设置随机数种子
+setup_seed(0)
+
+# 客户端数量，及每轮上传客户端数量
+N_clients = 10
+k = int(N_clients * args.frac)
+print("客户端总数: {}; 每轮上传客户端数量: {}".format(N_clients, k))
+
+(
+    X_train,
+    y_train,
+    X_test,
+    y_test,
+    net_dataidx_map,
+    traindata_cls_counts,
+) = partition_data(
+    dataset_name,
+    dataset_fpath,
+    logdir="./logs",
+    partition=partition,
+    n_parties=N_clients,
+    beta=beta,
+)
+print("beta: {}".format(beta))
+
+
+train_dl_global, test_dl, train_ds_global, test_ds_global = get_dataloader(
+    dataset_name, dataset_fpath, batch_size, test_bs=32
+)
+
+# 设置模型
+if dataset_name == "cifar10":
+    model_base = ModelFedCon("simple-cnn", out_dim=256, n_classes=10)
+elif dataset_name == "cifar100":
+    model_base = ModelFedCon("resnet50-cifar100", out_dim=256, n_classes=100)
+elif dataset_name == "tinyimagenet":
+    model_base = ModelFedCon("resnet50-cifar100", out_dim=256, n_classes=200)
+
+model_fpath = "./client_checkpoint"
+if not os.path.isdir(model_fpath):
+    os.mkdir(model_fpath)
+
+
+def inin_single_client(client_id):
+    model_ = copy.deepcopy(model_base)
+    optim_ = optim.SGD(model_.parameters(), lr=1e-2, momentum=0.9, weight_decay=1e-5)
+
+    trainloader, testloader, _, _ = get_dataloader(
+        dataset_name, dataset_fpath, batch_size, 32, net_dataidx_map[client_id]
+    )
+    # train_dl_global, test_dl_global, _, _ = get_dataloader(args.dataset, args.datadir, args.batch_size, 32)
+
+    return {
+        "model": model_,
+        "criterion": nn.CrossEntropyLoss(),
+        "optimizer": optim_,
+        "trainloader": trainloader,
+        # "testloader": testloader,
+        "testloader": test_dl,
+        "model_fname": "client{}_round_{}.pth".format(client_id, "{}"),
+        "client_id": client_id,
+        "device": device,
+        "model_fpath": model_fpath,
+        "epoch": args.local_epoch,
+        "dataset_name": dataset_name,
+        "strategy_name": args.strategy_name,
+        "trainer": MOONTrainer,
+        "save": False,
+        "display": False,
+        "log": False,
+    }
+
+
+if __name__ == "__main__":
+
+    print("初始化客户端")
+    client_lst = []
+    for client_id in range(N_clients):
+        c_conf = inin_single_client(client_id)
+        client_lst.append(MOONClient(c_conf))
+
+    s_conf = {
+        "Round": 100,
+        "N_clients": N_clients,
+        "model_fpath": model_fpath,
+        "iid": iid,
+        "dataset_name": dataset_name,
+        "strategy_name": args.strategy_name,
+        "log_suffix": args.suffix,
+    }
+    server_o = sc(conf=s_conf, Server=MOONServer, **{"client_lst": client_lst})
+    # server_o.max_workers = min(20, N_clients)
+    server_o.max_workers = 10
+    for ri in range(s_conf["Round"]):
+        loss, train_acc, test_acc = server_o.run(ri, k=k)
