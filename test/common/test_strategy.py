04ded2c7283c27f0147d1481573e8350ce71dd7e
wnma3mz <wnma3mz@163.com>
2022-02-15 10:35:08 +0800

    test per-commit-hooks.yaml

--- test/common/test_strategy.py
+++ test/common/test_strategy.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import torch.nn as nn
 
-from flearn.common import Trainer, init_strategy
+from flearn.common import Trainer, setup_strategy
 from flearn.common.utils import setup_seed
 
 setup_seed(0)
@@ -35,7 +35,7 @@ class MyTrainer(Trainer):
 if __name__ == "__main__":
     model_fpath = ""
     mytrainer = MyTrainer(MLP())
-    s = init_strategy("avg", None)
+    s = setup_strategy("avg", None)
     upload_res = s.client(mytrainer)
     global_res = s.server([upload_res], 0)
     revice_res = s.client_revice(mytrainer, {"w_glob": s.client(mytrainer)["params"]})
@@ -48,31 +48,31 @@ if __name__ == "__main__":
     assert (revice_res["fc.weight"] == global_res["w_glob"]["fc.weight"]).all()
     assert (revice_res["fc.bias"] == global_res["w_glob"]["fc.bias"]).all()
 
-    s = init_strategy("opt", None)
+    s = setup_strategy("opt", None)
     revice_res = s.client_revice(mytrainer, {"w_glob": s.client(mytrainer)["params"]})
 
     # todo
-    s = init_strategy("avgm", None)
+    s = setup_strategy("avgm", None)
     revice_res = s.client_revice(mytrainer, {"w_glob": s.client(mytrainer)["params"]})
 
-    s = init_strategy("bn", None)
+    s = setup_strategy("bn", None)
     upload_res = s.client(mytrainer)
     global_res = s.server([upload_res], 0)
 
-    s = init_strategy("sgd", None)
+    s = setup_strategy("sgd", None)
     upload_res = s.client(mytrainer)
     revice_res = s.client_revice([upload_res], 0)
 
-    s = init_strategy("lg", None)
+    s = setup_strategy("lg", None)
     upload_res = s.client(mytrainer)
     global_res = s.server([upload_res], 0)
     revice_res = s.client_revice(mytrainer, {"w_glob": s.client(mytrainer)["params"]})
 
-    s = init_strategy("lg_r", None)
+    s = setup_strategy("lg_r", None)
     upload_res = s.client(mytrainer)
     global_res = s.server([upload_res], 0)
     revice_res = s.client_revice(mytrainer, {"w_glob": s.client(mytrainer)["params"]})
 
-    s = init_strategy("pav", None)
+    s = setup_strategy("pav", None)
     upload_res = s.client(mytrainer)
     global_res = s.server([upload_res], 0)
